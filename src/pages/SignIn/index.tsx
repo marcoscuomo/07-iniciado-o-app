d29ed5d581e06ecf9f19817ef940b1eb41392175
Marcos Souza <bytecore@gmail.com>
2021-02-17 20:04:02 -0300

    Integrando o unform

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { 
   Image, 
   KeyboardAvoidingView, 
@@ -8,6 +8,8 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 
 import Input from '../../components/Input';
@@ -24,7 +26,14 @@ import {
 
 const SignIn: React.FC = () => {
 
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
+
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+    
+  }, []);
+
 
   return (
     <>
@@ -42,22 +51,31 @@ const SignIn: React.FC = () => {
               <View>
                 <Title>Faça seu logon</Title>
             </View>
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
-
-            <Button onPress={() =>{console.log('foi')}}>Entrar</Button>
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Entrar
+              </Button>
+            </Form>
 
             <ForgotPassword onPress={() => {}}>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
             </ForgotPassword>
           </Container>
         </ScrollView>
-        <CreateAccountButton onPress={() => navigation.navigate('SignUp') }>
-          <Icon name="log-in" size={20} color="#ff9000" />
-          <CreateAccountButtonText>Criar uma conta </CreateAccountButtonText>
-        </CreateAccountButton>
       </KeyboardAvoidingView>
+        
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp') }>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta </CreateAccountButtonText>
+      </CreateAccountButton>
+      
     </>
   );
   
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { 
   Image, 
   KeyboardAvoidingView, 
@@ -8,7 +8,8 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native'
-
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -22,6 +23,7 @@ import {
 
 const SignUp: React.FC = () => {
   
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
 
   return (
@@ -40,13 +42,13 @@ const SignUp: React.FC = () => {
               <View>
                 <Title>Crie sua conta</Title>
             </View>
+            <Form ref={formRef} onSubmit={(data) => {console.log(data)}} >
+              <Input name="name" icon="user" placeholder="Nome" />
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
 
-            <Input name="name" icon="user" placeholder="Nome" />
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
-
-            <Button onPress={() =>{console.log('foi')}}>Entrar</Button>
-
+              <Button onPress={() => formRef.current?.submitForm() }>Entrar</Button>
+            </Form>
             
           </Container>
         </ScrollView>
