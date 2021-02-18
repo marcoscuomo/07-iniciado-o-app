ff157b93a18210c4fe478700c49b4891340607fd
Marcos Souza <bytecore@gmail.com>
2021-02-17 22:28:19 -0300

    Foco e preenchimento

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,12 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
+import React, { 
+    useEffect, 
+    useRef, 
+    useImperativeHandle, 
+    forwardRef,
+    useState, 
+    useCallback,
+} 
+from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
 // import Icon from 'react-native-vector-icons/Feather'
@@ -25,6 +33,19 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({name, icon
     const { registerField, defaultValue = '', fieldName, error } = useField(name);
     const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+
+    const handleInputFocus = useCallback(() => {
+        setIsFocused(true);
+    }, []);
+
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false);
+
+        setIsFilled(!!inputValueRef.current.value);       
+    }, []);
+
     useImperativeHandle(ref, () => ({
         focus() {
             inputElementRef.current.focus();
@@ -48,13 +69,15 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({name, icon
     }, [fieldName, registerField]);
 
     return (
-        <Container> 
-            <Icon name={icon} size={20} color="#666360"/>
+        <Container isFocused={isFocused}> 
+            <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'}/>
 
             <TextInput 
                 ref={inputElementRef}
                 placeholderTextColor="#666360" 
                 defaultValue={defaultValue}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
                 onChangeText={(value) => {
                     inputValueRef.current.value = value;
                 }}
